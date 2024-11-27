0049237173eda3cee53ca10bd4bfcf7848554b11
vismaydeeps <chinavismay25@gmail.com>
2024-11-27 18:16:56 +0530

    commiting

---
+++ src/Header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div><h1>My Application</h1></div>
+  )
+}
---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div> 
+        <p>
+            <input type = "text" placeholder = "Enter email"></input>
+        </p>
+        <p>
+            <input type = "password" placeholder = "Enter password"></input>
+        </p>
+        <button>Login</button>
+    </div>
+  )
+}
